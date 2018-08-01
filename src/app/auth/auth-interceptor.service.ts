27d91b53b9e52c73b31cf39ecf8535bd2d63874f
Maartje Eyskens <meyskens@me.com>
2018-08-01 14:31:52 +0200

    log out on 419

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,19 +1,37 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
     constructor(private injector: Injector) { }
 
-    intercept(req, next) {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+      ): Observable<HttpEvent<any>> {
         const authService = this.injector.get(AuthService);
-        const authRequest = req.clone({
+        const authRequest: HttpRequest<any> = req.clone({
             // tslint:disable-next-line:max-line-length
             headers: req.headers.set('Authorization', 'Bearer ' + authService.token)
         });
-
-        return next.handle(authRequest);
+        return next.handle(authRequest).pipe(
+          map(event => {
+            return event;
+          }),
+          catchError(error => {
+            if (error.status == 419) {
+                authService.logout()
+            }
+            return throwError(error);
+          }),
+          finalize(() => {
+            //this.status.setHttpStatus(false);
+          })
+        )
     }
 }
